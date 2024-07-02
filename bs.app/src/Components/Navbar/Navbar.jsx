c6825c06a78340235364a84e902612f0062357ad
AlveIbrahim <alnfibrahim@gmail.com>
2024-07-02 07:50:03 +0500

    done

--- bs.app/src/Components/Navbar/Navbar.jsx
+++ bs.app/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import './Navbar.scss'
+import React, { useState, useEffect } from 'react';
+import './Navbar.scss';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className='left'>
-            <a href="/" className="logo">
+    const user = true;
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 350) {
+        setScrolled(true);
+        } else {
+        setScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <nav className={scrolled ? 'scrolled' : ''}>
+            <div className='left'>
+                <a href="/" className="logo">
                 <img src="/logo.png" alt="" />
                 <span>BS Services</span>
-            </a>
-        </div>
-        <div className='right'>
-            <ul>
-                <a href='/login'><li>Sign in</li></a>
-                <a href='/register'className="register"><li>Sign up</li></a>
-            </ul>
-        </div>
-    </nav>
-  )
-}
+                </a>
+            </div>
+            <div className='right'>
+                {user ? (
+                    <div className='user'>
+                        <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
+                        <span>Alnf</span>
+                        <Link to="/profile" className='profile'>
+                            <span>Profile</span>
+                        </Link>
+                    </div>
+                ) : (
+                    <ul>
+                        <li><a href='/login'>Sign in</a></li>
+                        <li><a href='/register' className="register">Sign up</a></li>
+                    </ul>
+                )}
+            </div>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
